416b123c68f64cb50f993f9a55a5d9d9d8030832
Shohjahon Murodov <shohjahonmurodov84@gmail.com>
2024-06-04 15:03:27 +0500

    😉

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:dennic_project/app/app.dart';
 import 'package:dennic_project/data/local/storage_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await StorageRepository.init();
 
-  runApp(const App());
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).whenComplete(() {
+    runApp(const App());
+  });
 }
